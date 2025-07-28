66e5e7b59aafe78e3ae4f72963703c45bad28300
Prasoon Gautam <prasoongautam84@gmail.com>
2025-07-28 23:34:22 +0545

    event model

--- lib/src/partners/databases/dbHelper.dart
+++ lib/src/partners/databases/dbHelper.dart
@@ -1,166 +1,29 @@
-// import 'dart:io';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:sqflite/sqflite.dart';
-// import 'package:evently/src/partners/models/evently_model.dart';
 
-// class DbHelper {
-//   late Database database;
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
 
-//   static DbHelper dbHelper = DbHelper();
+class EventDatabase{
+  static final EventDatabase instance = EventDatabase._init();
 
-//   // Define constants for the table names and columns
-//   final String tableName = 'events';
-//   final String tableActiveEvents = 'activeEvents';
-//   final String tableRegistration = 'registration';
+  static Database? _database;
 
-//   // Define column names for the 'events' table
-//   final String idColumn = 'docId';
-//   final String eventNameColumn = 'eventName';
-//   final String eventDescColumn = 'eventDesc';
-//   final String locationColumn = 'location';
-//   final String bannerPhotoColumn = 'bannerPhoto';
-//   final String hostNameColumn = 'hostName';
-//   final String eventDateColumn = 'eventDate';
+  EventDatabase._init();
 
-//   // Define column names for the 'activeEvents' table
-//   final String eventDocIdColumn = 'eventDocID'; // Foreign key to events table
+  Future<Database> get database async{
+    if(_database != null) return _database!;
 
-//   // Define column names for the 'registration' table
-//   final String registrationEventDocIdColumn = 'eventDocId'; // Foreign key to activeEvents table
-//   final String userNameColumn = 'userName';
-//   final String emailColumn = 'email';
+    _database = await _initDB('events.db');
+    return _database!;
+  }
 
-//   // Initialize the database
-//   Future<void> initDatabase() async {
-//     database = await connectToDatabase();
-//   }
+  Future<Database> _initDB(String filePath)async{
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
 
-//   // Connect to the database
-//   Future<Database> connectToDatabase() async {
-//     Directory directory = await getApplicationDocumentsDirectory();
-//     String path = '${directory.path}/evently.db';
+    return await openDatabase(path, version:1, onCreate: _createDB);
 
-//     // Create or open the database and define tables
-//     return openDatabase(
-//       path,
-//       version: 1,
-//       onCreate: (db, version) {
-
-//         //Tables for required tables in project.
-//         db.execute('''
-//           CREATE TABLE $tableName (
-//             $idColumn TEXT PRIMARY KEY,
-//             $eventNameColumn TEXT,
-//             $eventDescColumn TEXT,
-//             $locationColumn TEXT,
-//             $bannerPhotoColumn TEXT,
-//             $eventDateColumn TEXT
-//           )
-//         ''');
-
-//       //Tables for all the events in project.
-//         db.execute('''
-//           CREATE TABLE $tableActiveEvents (
-//             $eventDocIdColumn TEXT PRIMARY KEY,
-//             $idColumn TEXT,
-//             FOREIGN KEY ($idColumn) REFERENCES $tableName($idColumn)
-//           )
-//         ''');
-
-//       //Tables to store registration details.
-//         db.execute('''
-//           CREATE TABLE $tableRegistration (
-//             $registrationEventDocIdColumn TEXT,
-//             $userNameColumn TEXT,
-//             $emailColumn TEXT,
-//             FOREIGN KEY ($registrationEventDocIdColumn) REFERENCES $tableActiveEvents($eventDocIdColumn)
-//           )
-//         ''');
-//       },
-//       onUpgrade: (db, oldVersion, newVersion) {
-//         // Handle database upgrade if necessary
-//       },
-//     );
-//   }
-
-//   // Insert EventModel into the database
-//   Future<void> insertEventModel(EventModel eventModel) async {
-//     // Insert into events table
-//     await database.insert(tableName, eventModel.toJson(), conflictAlgorithm: ConflictAlgorithm.replace);
-
-//     // Insert each active event
-//     if (eventModel.activeEvents != null) {
-//       for (var activeEvent in eventModel.activeEvents!) {
-//         await database.insert(tableActiveEvents, activeEvent.toJson(), conflictAlgorithm: ConflictAlgorithm.replace);
-//       }
-//     }
-
-//     // Insert each registration
-//     if (eventModel.registration != null) {
-//       for (var registration in eventModel.registration!) {
-//         await database.insert(tableRegistration, registration.toJson(), conflictAlgorithm: ConflictAlgorithm.replace);
-//       }
-//     }
-//   }
-
-//   // Get all events from the events table
-//   Future<List<EventModel>> getAllEvents() async {
-//     List<Map<String, dynamic>> eventMaps = await database.query(tableName);
-//     return eventMaps.map((e) => EventModel.fromMap(e)).toList();
-//   }
-
-//   // Get active events by eventDocId
-//   Future<List<Map<String, dynamic>>> getActiveEventsByEventId(String eventDocId) async {
-//     return await database.query(
-//       tableActiveEvents,
-//       where: '$eventDocIdColumn = ?',
-//       whereArgs: [eventDocId],
-//     );
-//   }
-
-//   // Get registrations for a specific active event
-//   Future<List<Map<String, dynamic>>> getRegistrationsByEventDocId(String eventDocId) async {
-//     return await database.query(
-//       tableRegistration,
-//       where: '$registrationEventDocIdColumn = ?',
-//       whereArgs: [eventDocId],
-//     );
-//   }
-
-//   // Update event details
-//   Future<void> updateEvent(String docId, EventModel eventModel) async {
-//     await database.update(
-//       tableName,
-//       eventModel.toJson(),
-//       where: '$idColumn = ?',
-//       whereArgs: [docId],
-//     );
-//   }
-
-//   // Delete a specific event
-//   Future<void> deleteEvent(String docId) async {
-//     await database.delete(
-//       tableName,
-//       where: '$idColumn = ?',
-//       whereArgs: [docId],
-//     );
-//   }
-
-//   // Delete a specific active event
-//   Future<void> deleteActiveEvent(String eventDocId) async {
-//     await database.delete(
-//       tableActiveEvents,
-//       where: '$eventDocIdColumn = ?',
-//       whereArgs: [eventDocId],
-//     );
-//   }
-
-//   // Delete a registration
-//   Future<void> deleteRegistration(String eventDocId) async {
-//     await database.delete(
-//       tableRegistration,
-//       where: '$registrationEventDocIdColumn = ?',
-//       whereArgs: [eventDocId],
-//     );
-//   }
-// }
+  }
+  
+  
+}
--- lib/src/presentations/create_events/create_event_model.dart
+++ lib/src/presentations/create_events/create_event_model.dart
@@ -1,19 +1,36 @@
+final String tableEvents = 'events';
 
-class CreateEventModel {
+class EventFields{
+  static final List<String> values = [
+    id, eventTitle, targetAudience, description, hostName, eventDate, eventTime, location, bannerImg
+  ];
+
+  static final String id = '_id';
+  static final String eventTitle = 'eventtitle';
+  static final String targetAudience = 'targetAudience';
+  static final String description = 'description';
+  static final String hostName = 'hostName';
+  static final String eventDate = 'eventDate';
+  static final String eventTime = 'eventTime';
+  static final String location = 'location';
+  static final String bannerImg = 'bannerImg';
+}
+
+class Event {
   final int? id;
   final String eventTitle;
   final String targetAudience;
   final String description;
   final String hostName;
-  final String eventDate;
-  final String eventTime;
+  final DateTime eventDate;
+  final DateTime eventTime;
   final String location;
   final String bannerImg;
 
-  CreateEventModel({
+  const Event({
     this.id,
-    required this.targetAudience,
     required this.eventTitle,
+    required this.targetAudience,
     required this.description,
     required this.hostName,
     required this.eventDate,
@@ -22,30 +39,49 @@ class CreateEventModel {
     required this.bannerImg
   });
 
-  Map<String, dynamic> toMap(){
-    return {
-      'id': id,
-      'eventTitle': eventTitle,
-      'targetAudience': targetAudience,
-      'description': description,
-      'hostName': hostName,
-      'eventDate': eventDate,
-      'eventTime': eventTime,
-      'location': location,
-      'bannerImage':bannerImg
-    };
-  }
+  Event copy({
+    int? id,
+    String? eventTitle,
+    String? targetAudience,
+    String? description,
+    String? hostName,
+    DateTime? eventDate,
+    DateTime? eventTime,
+    String? location,
+    String? bannerImg
+  })=> 
+  Event(
+    id: id?? this.id,
+    eventTitle: eventTitle ?? this.eventTitle,
+    targetAudience: targetAudience ?? this.targetAudience, 
+    description: description?? this.description, 
+    hostName: hostName?? this.hostName, 
+    eventDate: eventDate?? this.eventDate, 
+    eventTime: eventTime?? this.eventTime, 
+    location: location?? this.location, 
+    bannerImg: bannerImg?? this.bannerImg);
 
-  factory CreateEventModel.fromMap(Map<String, dynamic> map){
-    return CreateEventModel(
-      id: map['id'],
-      eventTitle: map['eventTile'],
-      targetAudience: map['targetAudience'],
-      description: map['description'], 
-      hostName: map['hostName'], 
-      eventDate: map['eventDate'], 
-      eventTime: map['eventTime'], 
-      location: map['location'], 
-      bannerImg: map['bannerImg']);
-  }
+  static Event fromJson(Map<String, Object?> json) => Event(
+    id: json[EventFields.id] as int?,
+    eventTitle: json[EventFields.eventTitle] as String,
+    targetAudience: json[EventFields.targetAudience] as String,
+    description: json[EventFields.description] as String,
+    hostName: json[EventFields.hostName] as String,
+    eventDate: json[EventFields.eventDate] as DateTime,
+    eventTime: json[EventFields.eventTime] as DateTime,
+    location: json[EventFields.location] as String,
+    bannerImg: json[EventFields.bannerImg] as String
+  );
+
+  Map<String, Object?> toJson()=> {
+    EventFields.id: id,
+    EventFields.eventTitle: eventTitle,
+    EventFields.targetAudience: targetAudience,
+    EventFields.description: description,
+    EventFields.hostName: hostName,
+    EventFields.eventDate: eventDate,
+    EventFields.eventTime: eventTime,
+    EventFields.location:location,
+    EventFields.bannerImg: bannerImg
+  };
 }
